217f8141dbb012da78cf292809607d4077aa12db
Matthew Bentley <matthew.t.bentley@gmail.com>
2016-01-09 18:48:30 -0500

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,24 @@ extern crate r2d2;
 
 use nickel::{Nickel, HttpRouter, QueryString};
 use nickel::extensions::Redirect;
+use nickel::status::StatusCode;
 use rustc_serialize::json;
 use hyper::Client;
 use hyper::header::Connection;
+use hyper::status::StatusClass;
+use hyper::client::response::Response;
 use std::io::Read;
 use std::env;
 use r2d2::{NopErrorHandler, PooledConnection};
 use r2d2_postgres::{SslMode, PostgresConnectionManager};
 use nickel_postgres::{PostgresMiddleware, PostgresRequestExtensions};
 
-#[derive(RustcDecodable, RustcEncodable, Debug)]
+#[derive(RustcDecodable, Debug)]
 struct Data {
     data: User,
 }
 
-#[derive(RustcDecodable, RustcEncodable, Debug)]
+#[derive(RustcDecodable, Debug)]
 struct User {
     id: i32,
     email: String,
@@ -43,13 +46,13 @@ struct User {
     school: School,
 }
 
-#[derive(RustcDecodable, RustcEncodable, Debug)]
+#[derive(RustcDecodable, Debug)]
 struct School {
     id: i32,
     name: String,
 }
 
-#[derive(RustcDecodable, RustcEncodable, Debug)]
+#[derive(RustcDecodable, Debug)]
 struct TokenResp {
     access_token: String,
     token_type: String,
@@ -57,7 +60,7 @@ struct TokenResp {
     created_at: u64,
 }
 
-#[derive(RustcDecodable, RustcEncodable, Debug)]
+#[derive(RustcEncodable, Debug)]
 struct Payload {
     channel: String,
     username: String,
@@ -65,14 +68,19 @@ struct Payload {
     icon_emoji: String,
 }
 
+#[derive(Debug)]
+enum ApiError {
+    ClientError,
+    ServerError,
+}
 
-macro_rules! optry {
-    ( $x:expr ) => {
-        match $x {
-            Some(v) => v,
-            None => return None,
-        }
-    }
+#[derive(Debug)]
+enum RequestError {
+    Hyper(hyper::error::Error),
+    Io(std::io::Error),
+    JsonEnc(rustc_serialize::json::EncoderError),
+    JsonDec(rustc_serialize::json::DecoderError),
+    Api(ApiError),
 }
 
 macro_rules! env_err {
@@ -84,8 +92,33 @@ macro_rules! env_err {
     }
 }
 
+macro_rules! impl_from {
+    ( $from_t:path, $to_t:path, $enum_t:path ) => {
+        impl From<$from_t> for $enum_t {
+            fn from(err: $from_t) -> $enum_t {
+                $to_t(err)
+            }
+        }
+    }
+}
 
-fn do_request(code: &str) -> Option<Data> {
+impl_from!(hyper::error::Error, RequestError::Hyper, RequestError);
+impl_from!(std::io::Error, RequestError::Io, RequestError);
+impl_from!(rustc_serialize::json::DecoderError, RequestError::JsonDec,
+           RequestError);
+impl_from!(rustc_serialize::json::EncoderError, RequestError::JsonEnc,
+           RequestError);
+impl_from!(ApiError, RequestError::Api, RequestError);
+
+fn check_http_error(res: &Response) -> Result<(), ApiError> {
+    match res.status.class() {
+        StatusClass::ClientError => Err(ApiError::ClientError),
+        StatusClass::ServerError => Err(ApiError::ServerError),
+        _ => Ok(())
+    }
+}
+
+fn do_request(code: &str) -> Result<Data, RequestError> {
     let id = env_err!("ID");
     let secret = env_err!("SECRET");
     let redirect = env_err!("REDIRECT");
@@ -94,47 +127,52 @@ fn do_request(code: &str) -> Option<Data> {
                       id, secret, code, redirect);
 
     let client = Client::new();
-    let mut res = optry!(client.post(&url)
+    let mut res = try!(client.post(&url)
         .header(Connection::close())
-        .send().ok());
+        .send());
+
+    try!(check_http_error(&res));
 
     let mut body = String::new();
-    optry!(res.read_to_string(&mut body).ok());
+    try!(res.read_to_string(&mut body));
 
-    let token: TokenResp = optry!(json::decode(&body).ok());
+    let token: TokenResp = try!(json::decode(&body));
     let url2 = format!("https://my.mlh.io/api/v1/user?access_token={}",
                        token.access_token);
 
-    res = optry!(client.get(&url2)
+    res = try!(client.get(&url2)
         .header(Connection::close())
-        .send().ok());
+        .send());
+
+    try!(check_http_error(&res));
+
     body = String::new();
-    optry!(res.read_to_string(&mut body).ok());
+    try!(res.read_to_string(&mut body));
 
-    let person_data: Data = optry!(json::decode(&body).ok());
+    let person_data: Data = try!(json::decode(&body));
 
+    Ok(person_data)
+}
+
+fn slack_send(user: User) -> Result<(), RequestError> {
+    let url = env_err!("SLACKURL");
+    let client = Client::new();
     let payload = Payload {
         channel: "#hackcwru".to_string(),
         username: "Signup bot".to_string(),
         icon_emoji: ":hackcwru:".to_string(),
         text: format!("{} from {} has signed up!",
-                      person_data.data.first_name,
-                      person_data.data.school.name),
+                      user.first_name,
+                      user.school.name),
     };
+    let payload_str = try!(json::encode(&payload));
+    let res = try!(client.post(&url)
+        .body(&payload_str)
+        .send());
 
-    let payload_str = json::encode(&payload).unwrap();
+    try!(check_http_error(&res));
 
-    let slackurl = env_err!("SLACKURL");
-
-    let _res = optry!(client.post(&slackurl)
-                 .body(&payload_str)
-                 .send().ok());
-
-    Some(person_data)
-}
-
-fn slack_send(user: User) {
-//    let url = env
+    Ok(())
 }
 
 fn create_table(conn: PooledConnection<PostgresConnectionManager>) {
@@ -172,12 +210,23 @@ fn main() {
     create_table(dbpool.pool.clone().get().unwrap());
     app.utilize(dbpool);
 
-    app.get("/callback", middleware! { |request, response|
+    app.get("/callback", middleware! { |request, mut response|
         let conn = request.db_conn();
-        let user_data = match request.query().get("code") {
-            Some(s) => do_request(s),
-            None => None
-        }.unwrap().data;
+        let code = match request.query().get("code") {
+            Some(s) => s,
+            None => {
+                println!("No code, quitting");
+                return response.error(StatusCode::BadRequest, "Failed");
+            }
+        };
+        let user_data = match do_request(code) {
+            Ok(s) => s,
+            Err(e) => {
+                println!("Get user data failed with error: {:?}", e);
+                return response
+                    .error(StatusCode::InternalServerError, "Failed");
+            }
+        }.data;
 
         let r = conn.execute(
                 "INSERT INTO person (id, email, created_at, updated_at,
@@ -194,8 +243,14 @@ fn main() {
                   &user_data.school.id, &user_data.school.name]
             );
         match r {
-            Ok(v) => println!("{:?}", v),
-            Err(e) => println!("{:?}", e)
+            Ok(v) => {
+                println!("Add to database succeeded with status {:?}", v);
+                match slack_send(user_data) {
+                    Ok(_) => println!("Slack send worked"),
+                    Err(e) => println!("Slack send failed with error: {:?}", e)
+                };
+            },
+            Err(e) => println!("Add to database failed: {:?}", e)
         }
 
         return response.redirect("http://hack.cwru.edu/register.html")
